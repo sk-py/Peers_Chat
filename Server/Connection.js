efd91382f127c1999bf722dee4b8ccd962173810
sk-py <77550660+sk-py@users.noreply.github.com>
2024-01-10 20:23:33 +0530

    envs updated

--- Server/Connection.js
+++ Server/Connection.js
@@ -1,21 +1,10 @@
 const mongoose = require("mongoose");
-
-// const url =
-//   "mongodb+srv://peers_admin:admin123@cluster0.kkvhsnv.mongodb.net/peers?retryWrites=true&w=majority";
-
-// try {
-//   mongoose
-//     .connect(url)
-//     .then(() => console.log("Connection Established With Database!"))
-//     .catch((e) => console.log(`Error: ${e.message}`));
-// } catch (error) {
-//   console.log(error);
-// }
+require("dotenv").config();
 
 // const mongoose = require("mongoose");
 const requestModel = require("./Models/Requests");
-const url =
-  "mongodb+srv://peers_admin:admin123@cluster0.kkvhsnv.mongodb.net/peers?retryWrites=true&w=majority";
+const url = process.env.MONGO_URL;
+
 mongoose.connect(url).then(() => {
   console.log("Connection Established With Database!");
 });
